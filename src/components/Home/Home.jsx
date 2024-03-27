ea3ee039a50dc280652ebab591c9f04a5ca89775
Azim200117 <kamrunnaherronie@gmail.com>
2024-03-27 16:24:07 +0600

    Add routes and design the layouts

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,44 +1,19 @@
+import user from '../../assets/images/user.png'
+
 const Home = () => {
     return (
-        <div>
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
-                        </ul>
-                    </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    <a className="btn">Button</a>
-                </div>
+        <div className="bg-slate-100 h-screen flex flex-row items-center justify-center mt-5">
+            <div className="ml-10">
+                <h1 className="font-bold text-[70px] text-left">
+                    One Step <br /> Closer To Your <br /> <span className="text-[#7E90FE]">Dream Job</span>
+                </h1>
+                <p className="mt-6 text-[#757575] text-lg font-medium">
+                    Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.
+                </p>
+                <button className='btn mt-6 bg-[#7E90FE] text-white'>Get Started</button>
+            </div>
+            <div>
+                <img src={user} alt="" className='h-[600px]'/>
             </div>
         </div>
     );
